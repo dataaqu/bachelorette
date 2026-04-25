0e3758ee36ef0032d88fbade7da1c15763fef577
Data <datiobashvili1@gmail.com>
2026-04-25 05:52:21 +0400

    finish mvp version bachelorette

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://bachelorette-red.vercel.app',
   integrations: [tailwind({ applyBaseStyles: false })],
   build: {
     inlineStylesheets: 'auto',
