0e29e27638c49c1a06fdebbc34daec1fc257780c
Data <datiobashvili1@gmail.com>
2026-04-25 04:31:59 +0400

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,ts,md}'],
+  theme: {
+    extend: {
+      colors: {
+        bg: '#fef7e7',
+      },
+    },
+  },
+  plugins: [],
+};
